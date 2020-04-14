9d54fbc3feac34a458ba446b02dfe09caa2f464f
henriquelins <henriquelins@msn.com>
2020-04-14 10:54:15 -0300

    Exception handling - findById

---
+++ src/main/java/br/com/cea/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.cea.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("ResourceNotFound. Id " + id);
+		
+	}
+
+}
